b9a236953ef858e76e1431bf60cc12362fc7e425
Jan Koeppen <jfkoeppen@gmail.com>
2012-06-13 17:36:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hivemem::Application.config.secret_token = '91ef588bfc0be9e75a2264887b338b1a4a7453a1182310967a5366eaf62954f83e5b034423c5427c8f129f106796e60f0ff62fd1c8b2efc9a5bf2a7ab7d9d185'
